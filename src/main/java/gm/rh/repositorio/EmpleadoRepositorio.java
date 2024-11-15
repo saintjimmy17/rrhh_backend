1cf678b8b9d5d3900c2862c4697c15137186a4e1
Marian <italianomariano198@gmail.com>
2024-11-15 15:41:35 -0300

    CRUD con Java

---
+++ src/main/java/gm/rh/repositorio/EmpleadoRepositorio.java
@@ -0,0 +1,8 @@
+package gm.rh.repositorio;
+
+import gm.rh.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
+
+}
